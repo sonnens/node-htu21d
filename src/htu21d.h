eb6f4ed73967373e5248f3d5b1b45499df569de4
John Sonnenschein <john@siralos.org>
2014-10-13 12:27:06 -0700

    add more object properties to reflect the status register

--- src/htu21d.h
+++ src/htu21d.h
@@ -7,13 +7,38 @@
 using namespace v8;
 using namespace std;
 
+#define HTU21D_DEFAULT_ADDRESS		0x40
+
+#define HTU21D_RESOLUTION_12BITS	0b00000000
+#define HTU21D_RESOLUTION_8BITS		0b00000001
+#define HTU21D_RESOLUTION_10BITS	0b10000000
+#define HTU21D_RESOLUTION_11BITS	0b10000001
+#define HTU21D_END_OF_BATTERY		0b01000000
+#define HTU21D_ENABLE_ONCHIP_HEATER	0b00000100
+#define HTU21D_DISABLE_ONCHIP_HEATER	0b00000000
+#define HTU21D_ENABLE_OTP_RELOAD	0b00000000
+#define HTU21D_DISABLE_OTP_RELOAD	0b00000010
+#define HTU21D_RESERVED_BITMASK		0b00111000
+
+#define HTU21D_CMD_TEMPERATURE		0xF3
+#define HTU21D_CMD_HUMIDITY		0xF5
+#define HTU21D_CMD_WRITE_CONFIG		0xE6
+#define HTU21D_CMD_READ_CONFIG		0xE7
+#define HTU21D_CMD_SOFT_RESET		0xFE
+
+#define HTU21D_STATUS_BITMASK		0b00000011
+#define HTU21D_STATUS_TEMPERATURE	0b00000000
+#define HTU21D_STATUS_HUMIDITY		0b00000010
+#define HTU21D_STATUS_LSBMASK		0b11111100
+
 class Htu21d : public node::ObjectWrap {
  public:
   static void Init(v8::Handle<v8::Object> exports);
   int address;
   std::string device;
-  void reaquire(string* device, int address);
 
+  unsigned char user_reg;
+  void updateStatusReg();
  private:
   explicit Htu21d(char* device, int address);
   ~Htu21d();
@@ -25,6 +50,7 @@ class Htu21d : public node::ObjectWrap {
   static v8::Persistent<v8::Function> constructor;
 
   int fd;
+
 };
 
 #endif
--- src/htu21d_impl.cc
+++ src/htu21d_impl.cc
@@ -34,42 +34,80 @@ GetAddress(v8::Local<v8::String> property, const v8::AccessorInfo& info) {
   return Number::New(obj->address);
 }
 
-static void
-SetAddress(Local<String> property, Local<Value> value, const AccessorInfo& info) {
-  Htu21d* obj = node::ObjectWrap::Unwrap<Htu21d>(info.Holder());
-  if (value->IsInt32())
-    obj->reaquire(&(obj->device), (int) value->ToInt32()->Value());
-}
-
 static v8::Handle<Value>
 GetDevice(v8::Local<v8::String> property, const v8::AccessorInfo& info) {
   Htu21d* obj = node::ObjectWrap::Unwrap<Htu21d>(info.Holder()); 
   return String::New(obj->device.c_str());
 }
 
-static void
-SetDevice(Local<String> property, Local<Value> value, const AccessorInfo& info) {
-  Htu21d* obj = node::ObjectWrap::Unwrap<Htu21d>(info.Holder()); 
-  v8::String::Utf8Value v8str(value);
-  string str = string(*v8str);
-  obj->reaquire(&str, obj->address);
+void
+Htu21d::updateStatusReg() {
+  unsigned char buf = HTU21D_CMD_READ_CONFIG;
+
+  useconds_t delay = 15000;
+
+  if (write(fd,&buf,1) != 1) {
+    ThrowException(Exception::Error(String::New("Could not write to I2C")));
+    return;
+  }
+  usleep(delay);
+  int i = read(fd,&buf,1);
+  if (i != 1) {
+    ThrowException(Exception::Error(String::New("Could not read from I2C")));
+    return;
+  }
+  user_reg = buf;
 }
 
-void
-Htu21d::reaquire(string* dev, int addr) {
-
-  close(fd);
-
-  fd = open(dev->c_str(), O_RDWR);
-  if (fd < 0) {
-    ThrowException(Exception::Error(String::New("Failed to open the i2c bus")));
+static v8::Handle<Value>
+GetResolution(v8::Local<v8::String> property, const v8::AccessorInfo& info) {
+  Htu21d* obj = node::ObjectWrap::Unwrap<Htu21d>(info.Holder()); 
+  char temp, rh;
+  char res = obj->user_reg & 0b10000001;
+  switch (res) {
+    case HTU21D_RESOLUTION_12BITS:
+      temp = 14;
+      rh = 12;
+      break;
+    case HTU21D_RESOLUTION_8BITS:
+      temp = 12;
+      rh = 8;
+      break;
+    case HTU21D_RESOLUTION_10BITS:
+      temp = 10;
+      rh = 13;
+      break;
+    case HTU21D_RESOLUTION_11BITS:
+      temp = 11;
+      rh = 11;
+      break;
+    default:
+      temp = -1;
+      rh = -1;
+      break;
   }
-  if (ioctl(fd, I2C_SLAVE, addr) < 0) {
-    ThrowException(Exception::Error(String::New("Failed to acquire bus access and/or talk to slave.")));
-  }
-  address = addr;
-  device = *dev;
+  Local<Object> resobj = Object::New();
+  resobj->Set(String::NewSymbol("temperature"), Number::New(temp));
+  resobj->Set(String::NewSymbol("humidity"), Number::New(rh));
+  return resobj;
+}
 
+static v8::Handle<Value>
+GetBattery(v8::Local<v8::String> property, const v8::AccessorInfo& info) {
+  Htu21d* obj = node::ObjectWrap::Unwrap<Htu21d>(info.Holder()); 
+
+  // if we're reading the battery status, probably want to trigger a register read
+  obj->updateStatusReg();
+
+  return Boolean::New(obj->user_reg & HTU21D_END_OF_BATTERY);
+}
+
+static v8::Handle<Value>
+GetHeater(v8::Local<v8::String> property, const v8::AccessorInfo& info) {
+  Htu21d* obj = node::ObjectWrap::Unwrap<Htu21d>(info.Holder()); 
+
+  obj->updateStatusReg();
+  return Boolean::New(obj->user_reg & HTU21D_ENABLE_ONCHIP_HEATER);
 }
 
 void Htu21d::Init(Handle<Object> exports) {
@@ -82,9 +120,16 @@ void Htu21d::Init(Handle<Object> exports) {
 
   tpl->PrototypeTemplate()->Set(String::NewSymbol("temperature"),
       FunctionTemplate::New(Temp)->GetFunction());
-  //tpl->InstanceTemplate()->SetAccessor(String::New("resolution"), GetRes, SetRes);
-  tpl->InstanceTemplate()->SetAccessor(String::New("address"), GetAddress, SetAddress);
-  tpl->InstanceTemplate()->SetAccessor(String::New("devnode"), GetDevice, SetDevice);
+/*
+  tpl->PrototypeTemplate()->Set(String::NewSymbol("set_resolution"),
+      FunctionTemplate::New(Resolution)->GetFunction());
+*/
+  tpl->InstanceTemplate()->SetAccessor(String::New("address"), GetAddress);
+  tpl->InstanceTemplate()->SetAccessor(String::New("devnode"), GetDevice);
+  tpl->InstanceTemplate()->SetAccessor(String::New("resolution"), GetResolution);
+  tpl->InstanceTemplate()->SetAccessor(String::New("lowbattery"), GetBattery);
+  tpl->InstanceTemplate()->SetAccessor(String::New("heater"), GetHeater);
+
   constructor = Persistent<Function>::New(tpl->GetFunction());
   exports->Set(String::NewSymbol("Htu21d"), constructor);
 }
@@ -98,7 +143,7 @@ Htu21d::Humidity(const v8::Arguments& args) {
   char buf[3];
   useconds_t delay = 50000;
 
-  buf[0] = 0xF5 ;
+  buf[0] = HTU21D_CMD_HUMIDITY;
 
   if (write(obj->fd,buf,1) != 1) {
     ThrowException(Exception::Error(String::New("Could not write to I2C")));
@@ -127,7 +172,7 @@ Htu21d::Temp(const v8::Arguments& args) {
 
   char buf[3];
 
-  buf[0] = 0xF3;
+  buf[0] = HTU21D_CMD_TEMPERATURE;
 
   if (write(obj->fd,buf,1) != 1) {
     ThrowException(Exception::Error(String::New("Could not write to I2C")));
@@ -152,8 +197,7 @@ Htu21d::New(const Arguments& args) {
 
   if (args.IsConstructCall()) {
     Local<String> device = args[0]->IsUndefined() ? String::New("/dev/i2c-1") : args[0]->ToString();
-    int address = args[1]->IsUndefined() ? 0x40 : args[1]->IntegerValue();
-    //std::string dev_std_str(*v8::String::Utf8Value(device));
+    int address = args[1]->IsUndefined() ? HTU21D_DEFAULT_ADDRESS : args[1]->IntegerValue();
     Htu21d* obj = new Htu21d(*v8::String::AsciiValue(device), address);
     obj->Wrap(args.This());
     return args.This();
